43cae44df2b167b6c82ac12a17b62bb6e7341e4b
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-14 14:32:41 -0700

    Remove From Array 1st test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(ArrayList, ...popped) {
+    let index = ArrayList.indexOf(...popped);//Determine index number of the things to be deleted from the array
+    let removedIndex = ArrayList.splice(index,1);//Index to be removed
+    
+    return ArrayList;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
