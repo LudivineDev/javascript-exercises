7e9e684000c85daf270545dde832a4e910b4f8f1
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-14 15:58:01 -0700

    sum all all test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(leftnum, rightnum) {
+let total = 0;
+if (
+    leftnum<0 || 
+    rightnum<0 || 
+    !Number.isInteger(leftnum) ||
+    !Number.isInteger(rightnum)
+){
+    return `ERROR`;
+}
+for  (i=Math.min(leftnum, rightnum) ; i<=Math.max(leftnum, rightnum) ; i++ )
+    {
+    total +=i;
+}
+return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
