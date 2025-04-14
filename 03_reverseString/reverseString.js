6ecfde494674a0514ce9c2e533ba7298896294d9
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-14 13:25:55 -0700

    reverse string test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let SplitWord = word.split(``);
+let reverseword = SplitWord.reverse();
+let JoinReverseSplit = reverseword.join(``);
 
-};
+return JoinReverseSplit
+
+}
 
 // Do not edit below this line
 module.exports = reverseString;
