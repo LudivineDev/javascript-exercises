b714ac1f4adf073db9dd0cb624e082fe6827eb9d
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-04-15 12:16:10 -0700

    temp conversion passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const fToC = ((tempF - 32) * 5/9);
+return Math.round(fToC*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(tempC) {
+  const cToF = (tempC * 9/5 +32)
+  return Math.round(cToF*10)/10;
 };
 
 // Do not edit below this line
