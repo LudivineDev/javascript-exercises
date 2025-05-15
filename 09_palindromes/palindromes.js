5c5d07e3cfda6ac5699f4943ac52262230a7091f
LudivineDev <183041232+LudivineDev@users.noreply.github.com>
2025-05-15 15:18:59 -0700

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+const cleaned = words.toLowerCase().replace(/[^a-z0-9]/g,'');
+const reversed = cleaned.split('').reverse().join('');
+return cleaned === reversed;
 };
 
 // Do not edit below this line
